9812f534d51b0ad6748f32c77f988620fd066ba4
Devon Schmidt <devonschmidt53@gmail.com>
2023-04-27 14:53:47 -0500

    we made some products

---
+++ CSF2/Store/Product.cs
@@ -0,0 +1,70 @@
+﻿namespace Store
+{
+    public class Product
+    {
+
+        private readonly static string _policy = "All returns must be processed within 90 days of purchase.";
+
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsInStock { get; set; }
+
+        public Product(string name, decimal price, string description, bool isInStock)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            IsInStock = isInStock;
+        }//FQCTOR - fully qualified constructor
+
+        public Product() 
+        {
+            //you can assign default values to all of your
+            //properties here.
+            //Name = "TestProduct"
+            //Price = 0;
+        }//DEFAULT CTOR
+
+        public Product(string name, decimal price, string description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            //This constructor does not have a parameter for IsInStock. We COULD 
+            //leave that property unassigned, but it's safer to give it a default value.
+            //Let's assume that any Products added using this constructor ARE in stock.
+            IsInStock = true;
+        }//FQCTOR
+
+        public override string ToString()
+        {
+            return $"Product Name: {Name}\n" +
+                $"Price: {Price:c}\n" +
+                $"{Description}\n" +
+                $"{(IsInStock ? "" : "(out of stock)")}\n";
+        }
+        //REFRESHER - Static vs Instance
+
+        public static string DisplayReturnPolicy()
+        {
+            //return Name;//Static methods CANNOT access non-static (instance) class members
+            return _policy;//policy is static, so we CAN use it in static methods.
+        }
+        //Instance methods are used with respect to a specific "instance" of an object of this type.
+        //Static methods tend to have some general use that relates to the class as a whole.
+
+        public virtual decimal CalcHolidayDiscount()
+        {
+            //the virtual keyword allows, but does not require, this method to be overridden in child
+            //classes.
+            //By default, let's say all products get a 5% discount.
+            //100 * .05 = 5
+            //Price * (1 - Discount)
+            decimal newPrice = //Price - (Price * .05m);
+                Price * .95m;
+            return newPrice;
+        }
+    }//end class
+    
+}//end namespace
---
+++ CSF2/Store/Software.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public enum OS
+    {
+        Linux,
+        iOS,
+        PalmOS,
+        Unix,
+        Windows,
+        Mac,
+        Android
+    }
+    public class Software : Product
+    {
+        public OS OperatingSystem { get; set; }
+        public string Version { get; set; }
+        public bool IsSupported { get; set; }
+        public DateTime PublishDate { get; set; }
+
+
+        //Constructors
+        //copy the parameter list from the parent list
+        public Software(string name, decimal price, string description, bool isInStock,//from the parent list
+            OS operatingSystem, string version, bool isSupported, DateTime publishDate) 
+            : base(name,price,description,isInStock)
+        {
+            //We COULD handle all of the assignment here.
+            //However, we can also make use of the parent class's ctor using the ":base()" above.
+            //Name = name;
+            //Price = price;
+            //hande the assignment for this class's unique props
+            OperatingSystem = operatingSystem;
+            Version = version;
+            IsSupported = isSupported;
+            PublishDate = publishDate;
+        }
+
+        public override string ToString()
+        {
+            //base.ToString() will return the Product .ToString()
+            return base.ToString() + $"OS: {OperatingSystem}\t\tVersion: {Version}\n" +
+                $"{(char)169}{PublishDate.Year}\n" +
+                $"{(IsSupported ? "" : "Out of order")}" ;
+        }
+        //Let's suppose that software gets a total of 20% off during the holidays.
+        //MINI Lab
+        //override the CalcHolidayDiscount() to return a 20% discount on all software.
+        public override decimal CalcHolidayDiscount()
+        {
+            //return base.CalcHolidayDiscount();
+            return Price * .8m;
+        }
+    }//end class
+}//end namespace

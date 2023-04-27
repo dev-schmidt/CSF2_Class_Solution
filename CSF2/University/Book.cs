4997e1c736d74b588d3ee4bf7116f74c68445684
Devon Schmidt <devonschmidt53@gmail.com>
2023-04-27 11:00:31 -0500

    we reviewd containment

---
+++ CSF2/University/Book.cs
@@ -0,0 +1,43 @@
+﻿namespace University
+{
+    public class Book
+    {
+        //FIELDS
+        //A full prop is only required if you have business rules. So, we only need fields for 
+        //props with business rules.
+
+
+        //PROPERTIES
+        //Auto properties can be created with prop + tab
+
+        public string Title { get; set; }
+        //this syntax is shorthand for creating a property
+        //with the default getter and setter. At runtime, the compiler
+        //will take stock of these props and create the necesarry fields
+        //for data storage.
+
+        public string Publisher { get; set; }
+
+        public string ISBN { get; set; }
+
+        //CONSTRUCTORS
+
+        public Book(string title, string publisher, string isbn)
+        {
+            //Assignment
+            //Property = Parameter
+            //PascalCase = camelCase
+            //White = blue
+            Title = title;
+            Publisher = publisher;
+            ISBN = isbn;
+        }
+
+        //METHODS
+        public override string ToString()
+        {
+            //return base.ToString();)Namespace.ClassName
+            return Title;
+        }
+    }
+}
---
+++ CSF2/University/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University
+{
+    public class Student
+    {
+
+        //FIELDS
+        private double _gpa;
+
+        //PROPERTIES
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public Dept Major { get; set; }
+        public string ContactInfo { get; set; }
+
+        //GPA must be between 0 and 4
+        public double GPA
+        {
+            get { return _gpa; }
+            set
+            {
+                if (value >= 0 && value <= 4)
+                {
+                    _gpa = value;
+                }
+                else
+                {
+                    _gpa = 0;
+                }
+                //I have just an if and an else, no else ifs.
+                //I am doing the same thing in each block. Assigning something to _gpa
+                _gpa = (value >= 0 && value <= 4) ? value : 0;
+                //(condition to be evaluated) ? ifTrue : elseFalse;
+            }
+        }
+
+            //CONSTRUCTORS
+            public Student(string name, int id, double gpa, Dept major, string contactInfo)
+        {
+            Name = name;
+            ID = id;
+            GPA = gpa;
+            Major = major;
+            ContactInfo = contactInfo;
+        }
+
+
+        //METHODS
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}

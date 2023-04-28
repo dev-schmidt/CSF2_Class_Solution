889fed32835ef01bddb2ecca850e7731e6cc1f4d
Devon Schmidt <devonschmidt53@gmail.com>
2023-04-27 21:23:32 -0500

    recreated race and weapon enums + added WeaponType to Weapon class

--- Dungeon/DungeonLibrary/WeaponType.cs
+++ Dungeon/DungeonLibrary/WeaponType.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace DungeonLibrary
 {
-    //Weapon will inclue a WeaponType, so make this public, and make it an enum.
-    //add at least 5 types of weapons
-    //the intent of this is to provide potential expansions, such as a monster being weak to
-    //a particular type of weapon.
-    internal class WeaponType
+    public enum WeaponType
     {
+        Axe,
+        Bow,
+        Dagger,
+        Mace,
+        Sword
+
     }
 }
--- Dungeon/DungeonLibrary/race.cs
+++ Dungeon/DungeonLibrary/race.cs
@@ -4,55 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace DungeonLibrary
 {
-    //Make it a public enum, add at least 5 races to the mix.
-    //The intent is for the Player object to have a property of this type.
-    //The enemies/monsters will NOT have this.
-    public class Race
+    //MAKE IT PUBLIC
+    public enum Race
     {
-        enum CharacterRace
-        {
-            Orc,
-            Human,
-            Elf,
-            Dwarf,
-            Khajiit
-        }
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("The race I have chosen is " +
-              CharacterRace[] races = Enum.GetValues<CharacterRace>();
-            foreach (CharacterRace item in races)
-            {
-                Console.WriteLine($"{(int)item + 1} - {item}");
-            }
-            Console.Write("\nChoose a race from the list above (enter the number only): " +
-                "");
-            int userChoice = int.Parse( Console.ReadLine());
-            CharacterRace userRace = (CharacterRace)(userRace - 1);
-            switch (userChoice)
-            {
-                case CharacterRace.Orc:
-                    Console.WriteLine("\nIt ain't easy being green.");
-                    break;
-                case CharacterRace.Human:
-                    Console.WriteLine("\nHuman? You're already a human failure!");
-                    break;
-                case CharacterRace.Elf:
-                    Console.WriteLine("\nOooh, you're one of those guys...");
-                    break;
-                case CharacterRace.Dwarf:
-                    Console.WriteLine("\nYes dude! Dwarves are sick.");
-                    break;
-                case CharacterRace.Khajiit:
-                    Console.WriteLine("\nMay your road lead you to warm sands.");
-                    break;
-
-            }//end switch
-        }//end main
-    }//end class
-}//end namespace
+        Argonian,
+        Elf,
+        Human,
+        Khajiit,
+        Orc
+    }
+}
